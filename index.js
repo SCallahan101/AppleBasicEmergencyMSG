a9cd070ccd592fba33a629eff84b301ec24707bd
Shannon Callahan <Shannonc619@gmail.com>
2022-05-23 10:09:05 -0600

    Enact the progress from 1 to 2
    Created a connection between first page to second page for emergency initiation.

---
+++ index.js
@@ -0,0 +1,12 @@
+// let whichData = document.getElementById("emergyButton").addEventListener("click", initiateEmergency);
+
+function initiateEmergency(){
+    // let initiate = document.getElementById("emergyButton").addEventListener("click");
+    // if(initiate == "emergency"){
+    //     console.log("It works: the emergency call has initiate!");
+    // } else{
+    //     console.log("Still need to fix somewhere...");
+    // }
+    console.log("It works: the emergency call has initiate!");
+} 
+// initiateEmergency();
