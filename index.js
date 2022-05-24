4e0127fb0ffd00ee22951e2b779e5cb47c78855b
Shannon Callahan <Shannonc619@gmail.com>
2022-05-24 17:39:17 -0600

    Enacted pathway to Non-Emergency
    Added new pathway to nonEmergency and edited some button choices.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function firstStep(){
     x.innerHTML = `<h1>testing first step</h1>
     <div id="categoriesContainer" class="subContainer">
         <button class="emergyCategoryCall">Mental-Health</button>
-        <button class="emergyCategoryCall">Non-Emergency</button>
+        <button class="emergyCategoryCall" onclick="initiateNonEmergency();">Non-Emergency</button>
         <button class="emergyCategoryCall" onclick="initiateEmergency();">Emergency</button>
     </div><p>and Cancel</p>`;
 }
@@ -25,12 +25,26 @@ function initiateEmergency(){
         <button type="button" class="natureOfEmergency">Someone's hurt</button>
         <button type="button" class="natureOfEmergency">I'm injured</button>
         <button type="button" class="natureOfEmergency">Shooting situation</button>
-        <button type="button" class="natureOfEmergency">Vehicle Accident</button>
+        <button type="button" class="natureOfEmergency">Report a Fire</button>
+        <button type="button" class="natureOfEmergency">Crime in Progress</button>
     </div>`;
     console.log("It works: the emergency call has initiate!");
 } 
 // initiateEmergency();
 
 // NON-EMERGENCY PROCESS
-
+    function initiateNonEmergency(){
+        const x = document.getElementById("containter");
+    x.innerHTML = `<h1>Emergency first step</h1>
+    <div id="stageOfEmergency" class="subContainer">
+        <button type="button" class="natureOfNonEmergency">Need Help</button>
+        <button type="button" class="natureOfNonEmergency">Vehicle Accident</button>
+        <button type="button" class="natureOfNonEmergency">Report a Crime</button>
+        <button type="button" class="natureOfNonEmergency">"Still thinking"</button>
+    </div>`;
+    console.log("It works: the emergency call has initiate!");
+    }
 // MENTAL HEALTH PROCESS
+    function initiateMentalHealth(){
+
+    }
