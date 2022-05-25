77c506ec13d6f87f8930179c3c035a86f7b8430a
Shannon Callahan <Shannonc619@gmail.com>
2022-05-24 18:17:24 -0600

    Enacted two more pathways
    Added pathway to Non-Emergency and Mental Health. Both set up with basic buttons.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function firstStep(){
     const x = document.getElementById("containter");
     x.innerHTML = `<h1>testing first step</h1>
     <div id="categoriesContainer" class="subContainer">
-        <button class="emergyCategoryCall">Mental-Health</button>
+        <button class="emergyCategoryCall" onclick="initiateMentalHealth();">Mental-Health</button>
         <button class="emergyCategoryCall" onclick="initiateNonEmergency();">Non-Emergency</button>
         <button class="emergyCategoryCall" onclick="initiateEmergency();">Emergency</button>
     </div><p>and Cancel</p>`;
@@ -35,16 +35,24 @@ function initiateEmergency(){
 // NON-EMERGENCY PROCESS
     function initiateNonEmergency(){
         const x = document.getElementById("containter");
-    x.innerHTML = `<h1>Emergency first step</h1>
-    <div id="stageOfEmergency" class="subContainer">
-        <button type="button" class="natureOfNonEmergency">Need Help</button>
-        <button type="button" class="natureOfNonEmergency">Vehicle Accident</button>
-        <button type="button" class="natureOfNonEmergency">Report a Crime</button>
-        <button type="button" class="natureOfNonEmergency">"Still thinking"</button>
-    </div>`;
-    console.log("It works: the emergency call has initiate!");
+        x.innerHTML = `<h1>Non-Emergency first step</h1>
+        <div id="stageOfEmergency" class="subContainer">
+            <button type="button" class="natureOfNonEmergency">Need Help</button>
+            <button type="button" class="natureOfNonEmergency">Vehicle Accident</button>
+            <button type="button" class="natureOfNonEmergency">Report a Crime</button>
+            <button type="button" class="natureOfNonEmergency">"Still thinking"</button>
+        </div>`;
+        console.log("It works: the non-emergency call has initiate!");
     }
 // MENTAL HEALTH PROCESS
     function initiateMentalHealth(){
-
+        const x = document.getElementById("containter");
+        x.innerHTML = `<h1>Mental Health first step</h1>
+        <div id="stageOfEmergency" class="subContainer">
+            <button type="button" class="natureOfMentalHealth">Wellness Check</button>
+            <button type="button" class="natureOfMentalHealth">Lost Person</button>
+            <button type="button" class="natureOfMentalHealth">Sign of Distress</button>
+            <button type="button" class="natureOfMentalHealth">Report unwell Person</button>
+        </div>`;
+        console.log("It works: the Mental Health call has initiate!");
     }
